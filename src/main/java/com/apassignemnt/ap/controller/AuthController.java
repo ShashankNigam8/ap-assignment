1dc6e4781e5a95839d6f219d3f9078269d51ff15
shashanknigam <snigam@wabiproject.com>
2023-08-20 15:44:12 +0530

    [AP-Assignment] corrected spelling mistake

--- src/main/java/com/apassignemnt/ap/controller/AuthController.java
+++ src/main/java/com/apassignemnt/ap/controller/AuthController.java
@@ -1,10 +1,7 @@
 package com.apassignemnt.ap.controller;
 
-import com.apassignemnt.ap.Constants;
 import com.apassignemnt.ap.entity.AuthRequest;
 import com.apassignemnt.ap.service.AuthService;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
 @RestController
 @SecurityRequirement(name = "bearerAuth")
-public class AauthController {
+public class AuthController {
 
     private AuthService authService;
 
-    public AauthController(AuthService authService) {
+    public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
