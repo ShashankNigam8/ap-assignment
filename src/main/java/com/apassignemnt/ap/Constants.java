0d769d71614eb9b8661cbc65d5a08be298f066ea
shashanknigam <snigam@wabiproject.com>
2023-08-20 15:42:57 +0530

    [AP-Assignment] changed expiration time of token to 1 hr

--- src/main/java/com/apassignemnt/ap/Constants.java
+++ src/main/java/com/apassignemnt/ap/Constants.java
@@ -2,7 +2,7 @@ package com.apassignemnt.ap;
 
 public class Constants {
     public static final String SECRET_KEY = "eW91clNlY3JldEtleQ==";
-    public static final long EXPIRATION_TIME = 600000;
+    public static final long EXPIRATION_TIME = 6000000; // 1 hr
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
 
