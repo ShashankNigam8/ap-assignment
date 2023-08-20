6d5b50fff4ea5cad87ecbd895c26ca8c07eddb11
shashanknigam <snigam@wabiproject.com>
2023-08-20 15:43:53 +0530

    [AP-Assignment] created RestTemplate bean

--- src/main/java/com/apassignemnt/ap/ApiAssignmentApplication.java
+++ src/main/java/com/apassignemnt/ap/ApiAssignmentApplication.java
@@ -2,12 +2,21 @@ package com.apassignemnt.ap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiAssignmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiAssignmentApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		// Do any additional configuration here
+		return builder.build();
 	}
 
 }
