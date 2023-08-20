30ad0397a12919b7cad3147f28266f77042d2451
shashanknigam <snigam@wabiproject.com>
2023-08-20 15:42:21 +0530

    [AP-Assignment] created NoCountryFound Exception

--- src/main/java/com/apassignemnt/ap/exception/NoCountryFoundException.java
+++ src/main/java/com/apassignemnt/ap/exception/NoCountryFoundException.java
@@ -2,10 +2,10 @@ package com.apassignemnt.ap.exception;
 
 import java.util.UUID;
 
-public class JwtValidationException extends RuntimeException {
+public class NoCountryFoundException extends RuntimeException {
     private String errorId;
 
-    public JwtValidationException(String message) {
+    public NoCountryFoundException(String message) {
         super(message);
         this.errorId = UUID.randomUUID().toString();
     }
